662ecf29d8c7c44bdaf4a0514b6e1b6c332511e5
Manminder <ManminderSingh@edu.sait.ca>
2024-11-12 17:59:14 -0700

    more

--- first.c
+++ first.c
@@ -3,8 +3,37 @@
 int main() {
     printf("Hello, World!\n");
     printf("This is my first C program.\n");
-    
+    printf("Manminder Singh\n");
+
+// this is a  comment in C
+/* this is 
+multiline comment */
+
+
 
 
     return 0;
 }
+
+
+// escape sequences
+// \n - new line
+// \t - tab
+// \b - backspace
+// \r - carriage return
+// \f - form feed
+// \\ - backslash
+// \' - single quote
+// \" --" - double quote quote is used to escape double quote
+// \? - question mark
+// \0 - null character
+// \a - alert           
+// \v - vertical tab
+// \ooo - octal number
+// \xhh - hexadecimal number                                
+// \uhhhh - unicode character
+// \Uhhhhhhhh - unicode character
+// \xhhhh - unicode character   
+// \Xhhhhhhhh - unicode character
+// \0 - null character
+
